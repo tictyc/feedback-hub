969483e0c70ad27ac507e1d590b1912e42b493ef
Tim Mathis <timmathis75@gmail.com>
2017-12-20 14:28:48 +0100

    package.json updates

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 //import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
 });
 
-app.listen(5000);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
