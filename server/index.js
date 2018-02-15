a0c0fecc3b496b79f0e2772f2f32e61baeecfdee
Tim Mathis <timmathis75@gmail.com>
2018-02-15 18:42:03 +0100

    MongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+require("./models/user");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
