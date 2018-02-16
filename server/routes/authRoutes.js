d00554b992615d559e13798d8f5851721b0531b4
Tim Mathis <timmathis75@gmail.com>
2018-02-16 17:13:54 +0100

    facebook & google authentification now implemented

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,4 +9,22 @@ module.exports = app => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", {
+      scope: ["public_profile", "email"]
+    })
+  );
+
+  app.get("/auth/facebook/callback", passport.authenticate("facebook"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
